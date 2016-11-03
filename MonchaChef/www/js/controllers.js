2cf735b1c0bf9b2d61c0c80138412fee1b091e44
gsegura96 <gsegura96@openmailbox.org>
2016-11-03 15:39:04 -0600

    Los ingredientes son correctamente agregados a la receta

--- MonchaChef/www/js/controllers.js
+++ MonchaChef/www/js/controllers.js
@@ -111,60 +111,65 @@ function ($scope, $stateParams, MenuService) {
    $scope.dish = {recipe:[],ingredientsList:[]};
    $scope.stepToAdd = {};
    $scope.dish.prepTime=0;
+   $scope.newIngredient={}
    MenuService.getIngredients().then(function(res){
       $scope.ingredients = res;
       console.log(res);
 
    });
    //$scope.dish.ingredients = $scope.dish.ingredients + [1 ,2 ];
-   $scope.addIngredient = function(ingredient) {
-      if(ingredient!=undefined){
-         if($scope.dish.ingredients.indexOf(ingredient)<0){
-            $scope.dish.ingredients.push(ingredient);}
-            else{
-               alert("The ingredient is all ready in the recipe");
-            }}
-            else{
-               alert("First select an ingredient");
-            }
+   $scope.addIngredient = function() {
+      if($scope.newIngredient.name!=undefined || $scope.newIngredient.quantity!=undefined ){
+         var ingredient = {"name":$scope.newIngredient.name, "quantity":$scope.newIngredient.quantity}
+         if($scope.dish.ingredientsList.indexOf(ingredient)<0){
+            $scope.dish.ingredientsList.push(ingredient);
+            //alert(ingredient.name, ingredient.quantity);
 
-            //$scope.dish.ingredientToAdd = "";
          }
-         $scope.addStep = function(stepToAdd) {
-            if(stepToAdd.title != undefined && stepToAdd.aproxTime != undefined && stepToAdd.description != undefined){
-               if($scope.dish.recipe.indexOf(newStep)<0){var newStep={title:stepToAdd.title, aproxTime:stepToAdd.aproxTime,description:stepToAdd.description}
-               //alert(newStep);
-               $scope.dish.recipe.push(newStep);
-               $scope.stepToAdd.title="";
-               $scope.stepToAdd.description="";
-               $scope.stepToAdd.aproxTime="";}
-               else {
-                  alert("The step is all ready in the recipe")
-               }
-            }
+         else{
+            alert("The ingredient is all ready in the recipe");
+         }}
+         else{
+            alert("First select an ingredient");
+         }
+
+         //$scope.dish.ingredientToAdd = "";
+      }
+      $scope.addStep = function(stepToAdd) {
+         if(stepToAdd.title != undefined && stepToAdd.aproxTime != undefined && stepToAdd.description != undefined){
+            if($scope.dish.recipe.indexOf(newStep)<0){var newStep={title:stepToAdd.title, aproxTime:stepToAdd.aproxTime,description:stepToAdd.description}
+            //alert(newStep);
+            $scope.dish.recipe.push(newStep);
+            $scope.stepToAdd.title="";
+            $scope.stepToAdd.description="";
+            $scope.stepToAdd.aproxTime="";}
             else {
-               alert("Fill all the inputs")
+               alert("The step is all ready in the recipe")
             }
          }
-         $scope.resetDish = function() {
-            $scope.dish = {recipe:[],ingredients:[]};
-            //$scope.dish.ingredientToAdd=" ";
+         else {
+            alert("Fill all the inputs")
          }
-         $scope.postDish = function() {
-            //alert($scope.dish);
-            MenuService.postDish($scope.dish);
-         }
-         $scope.totalTime = function() {
-            var total =0;
-            var len = $scope.dish.recipe.length;
-            if(len>0){
-               for (var i = 0; i < len; i++) {
-                  total+=$scope.dish.recipe[i].aproxTime;
-               }
+      }
+      $scope.resetDish = function() {
+         $scope.dish = {recipe:[],ingredients:[]};
+         //$scope.dish.ingredientToAdd=" ";
+      }
+      $scope.postDish = function() {
+         //alert($scope.dish);
+         MenuService.postDish($scope.dish);
+      }
+      $scope.totalTime = function() {
+         var total =0;
+         var len = $scope.dish.recipe.length;
+         if(len>0){
+            for (var i = 0; i < len; i++) {
+               total+=$scope.dish.recipe[i].aproxTime;
             }
-            $scope.dish.prepTime = total;
-            return $scope.dish.prepTime;
          }
+         $scope.dish.prepTime = total;
+         return $scope.dish.prepTime;
+      }
 
 
-      }])
+   }])
